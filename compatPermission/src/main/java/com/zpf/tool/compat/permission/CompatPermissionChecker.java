5c2066a43c0a4cd34bd18452b09394e1b3bc12db
ZPF <pengfei.zhou@andall.com>
2020-10-14 11:44:39 +0800

    进度保存

--- compatPermission/src/main/java/com/zpf/tool/compat/permission/CompatPermissionChecker.java
+++ compatPermission/src/main/java/com/zpf/tool/compat/permission/CompatPermissionChecker.java
@@ -12,6 +12,7 @@ import com.zpf.tool.permission.PermissionChecker;
  * 使用android.support.v4.app.Fragment检查权限
  * Created by ZPF on 2018/8/22.
  */
+@TargetApi(Build.VERSION_CODES.M)
 public class CompatPermissionChecker extends PermissionChecker<Fragment> {
 
     @Override
@@ -19,19 +20,22 @@ public class CompatPermissionChecker extends PermissionChecker<Fragment> {
         return target != null && target.getContext() != null;
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
     @Override
-    protected boolean hasPermission(Fragment target, String p) {
+    protected boolean shouldShowRequestPermissionRationale(Fragment target, String p) {
+        return target.shouldShowRequestPermissionRationale(p);
+    }
+
+    @Override
+    protected void realRequestPermissions(Fragment target, String[] p, int code) {
+        target.requestPermissions(p, code);
+    }
+
+    @Override
+    public boolean hasPermission(Fragment target, String p) {
         try {
             return target.getContext().checkSelfPermission(p) == PackageManager.PERMISSION_GRANTED;
         } catch (Exception e) {
             return false;
         }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    protected void requestPermissions(Fragment target, String[] p, int code) {
-        target.requestPermissions(p, code);
     }
 }
