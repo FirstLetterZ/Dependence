d3dd1773e4166292a493b1fd5ee0dd5186ada023
pengfei.zhou01 <pengfei.zhou01@weimob.com>
2022-03-28 17:46:06 +0800

    进度保存

--- aaa/src/main/java/com/zpf/aaa/MainActivity.java
+++ aaa/src/main/java/com/zpf/aaa/MainActivity.java
@@ -1,11 +1,48 @@
 package com.zpf.aaa;
 
-import androidx.activity.ComponentActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.window.SplashScreenView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import com.zpf.tool.text.MoneyTextRule;
+import com.zpf.tool.text.RuleTextWatcher;
+import com.zpf.tool.text.SplitTextRule;
 
 /**
  * @author Created by ZPF on 2021/3/30.
  */
-public class MainActivity extends ComponentActivity {
+public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+        final RuleTextWatcher ruleTextWatcher = new RuleTextWatcher();
+        ruleTextWatcher.addRuleChecker(new MoneyTextRule(12));
+        SplitTextRule str = new SplitTextRule(new int[]{3}, new char[]{','});
+        str.setStartPoint('.', 0);
+        ruleTextWatcher.addRuleChecker(str);
+//        et_money.addTextChangedListener(new MoneyTextWatcher(1));
+
+        EditText etMoney = findViewById(R.id.et_money);
+        etMoney.addTextChangedListener(ruleTextWatcher);
+
+//        etMoney.addTextChangedListener(new SplitTextWatcher(new int[]{3, 4, 4}));
+        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TAG", "getDisplay=" + ruleTextWatcher.getDisplay() + ";getValue=" + ruleTextWatcher.getValue());
+            }
+        });
+    }
 }
